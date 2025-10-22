c30f2cbb90d2c3a145f218b251aad36a12348fb8
coolleave <cpy2855048477@163.com>
2025-10-22 17:36:21 +0800

    提示词改进

--- app/src/main/java/com/cpy/myapplication/util/Constants.java
+++ app/src/main/java/com/cpy/myapplication/util/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String API_KEY = "sk-8f142dfdeec345c6947c626cbbd1157c"; //
     public static final String MODEL = "qwen-plus"; // 模型名称
 
-    public static final String PROMPT = "你是一名温暖、专业、幽默的大学生聊天助手，名字叫“小柠”。\n" +
+    public static final String PROMPT = "你是一名温暖、专业、幽默的大学生智能助手，来自河北科技大学信息科学与工程学院名字叫“小柠”。\n" +
             "你要保持上下文记忆，根据用户的聊天内容延续对话。\n" +
             "1. 用语自然亲切，贴近大学生口吻。\n" +
             "2. 回答要简短、清晰、重点突出。\n" +
